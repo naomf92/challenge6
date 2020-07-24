8115ef7e4cc4562f1394c431eef833c7132c796e
naomi <iam.naomi92@gmail.com>
2020-07-24 21:15:00 +0900

    challenge6

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let score = 0;
 function showScore(){
 //スコア表示の処理をここに記述
 document.getElementById('score-message').innerHTML= score;
+
 if (score == 0) {
  
 } else if (score == 1) {
@@ -29,15 +30,28 @@ const quiz_1 = document.getElementById('quiz-1');
 console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
 
 let answer = 'b';
-switch (answer) {
-    case 'a':
-    case 'c':
-        console.log('残念');
-        break;
-    case 'b':
-        console.log('正解');
-        break;
+if (quiz_1.answer.value == 'a') {
+    console.log('不正解です');
+} else if (quiz_1.answer.value == 'b') {
+    console.log('正解です');
+} else if (quiz_1.answer.value == 'c') {
+    console.log('不正解です');
+} else {
+    console.log('未回答です');
 }
+
+// switch (quiz_1.answer.value) {
+//     case 'a':
+//     case 'c':
+//         console.log('不正解です');   
+//         break;
+//     case 'b':
+//         console.log('正解です');
+//         break;
+//     default:
+//         console.log('未回答です');
+// }
+
 }
 
 function answerQuiz2(){
