eae736bd7f4f8128f7c4e50ba241a316ce6e4730
naomi <iam.naomi92@gmail.com>
2020-07-26 11:59:07 +0900

    challenge6

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function showScore(){
 document.getElementById('score-message').innerHTML= score;
 
 if (score == 0) {
- 
+    document.getElementById('score-message').innerHTML= score +'点: '+'残念';
 } else if (score == 1) {
-    
+    document.getElementById('score-message').innerHTML= score +'点: '+'まあまあ';
 } else if (score == 2) {
-
+    document.getElementById('score-message').innerHTML= score +'点: '+'おしい！';
 } else if (score == 3) {
-
+    document.getElementById('score-message').innerHTML= score +'点: '+'すばらしい！';
 } else {
 
 }
@@ -32,10 +32,13 @@ console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
 let answer = 'b';
 if (quiz_1.answer.value == 'a') {
     console.log('不正解です');
+    console.log('現在のトータルスコア：' + score);
 } else if (quiz_1.answer.value == 'b') {
     console.log('正解です');
+    console.log('現在のトータルスコア：' + ++score);
 } else if (quiz_1.answer.value == 'c') {
     console.log('不正解です');
+    console.log('現在のトータルスコア：' + score);
 } else {
     console.log('未回答です');
 }
@@ -56,8 +59,40 @@ if (quiz_1.answer.value == 'a') {
 
 function answerQuiz2(){
 //問題2の処理をここに記述
+const quiz_2 = document.getElementById('quiz-2');
+console.log('2問目の' + quiz_2.answer.value + 'を選択しました');
+
+let answer = 'a';
+if (quiz_2.answer.value == 'a') {
+    console.log('正解です');
+    console.log('現在のトータルスコア：' + ++score);
+} else if (quiz_2.answer.value == 'b') {
+    console.log('不正解です');
+    console.log('現在のトータルスコア：' + score);
+} else if (quiz_2.answer.value == 'c') {
+    console.log('不正解です');
+    console.log('現在のトータルスコア：' + score);
+} else {
+    console.log('未回答です');
+}
 }
 
 function answerQuiz3(){
 //問題3の処理をここに記述
+const quiz_3 = document.getElementById('quiz-3');
+console.log('3問目の' + quiz_3.answer.value + 'を選択しました');
+
+let answer = 'c';
+if (quiz_3.answer.value == 'a') {
+    console.log('不正解です');
+    console.log('現在のトータルスコア：' + score);
+} else if (quiz_3.answer.value == 'b') {
+    console.log('不正解です');
+    console.log('現在のトータルスコア：' + score);
+} else if (quiz_3.answer.value == 'c') {
+    console.log('正解です');
+    console.log('現在のトータルスコア：' + ++score);
+} else {
+    console.log('未回答です');
+}
 }
